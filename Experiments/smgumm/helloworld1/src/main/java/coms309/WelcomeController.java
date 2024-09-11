f1a0442bcc43de73a7c35a3fd2790975dbe77a8f
SamGumm <98501677+SamGumm@users.noreply.github.com>
2024-09-11 11:30:46 -0500

    editing experiment 1

--- Experiments/smgumm/helloworld1/src/main/java/coms309/WelcomeController.java
+++ Experiments/smgumm/helloworld1/src/main/java/coms309/WelcomeController.java
@@ -8,12 +8,39 @@ import org.springframework.web.bind.annotation.PathVariable;
 class WelcomeController {
 
     @GetMapping("/")
-    public String welcome() {
-        return "Good news everyone!";
+    public String intro() {
+        return """
+                Here is a list of available endpoints:
+                /{name} to be greeted with a personalized message!
+                /art to see some lovely ASCII art!""";
     }
 
     @GetMapping("/{name}")
     public String welcome(@PathVariable String name) {
         return "Hello and welcome to COMS 309: " + name;
+    }
+
+    @GetMapping("/art")
+    public String art() {
+        return """
+                      _=====_                                _=====_
+                     / _____ \\                             / _____ \\
+                    +.-'_____'-.---------------------------.-'_____'-.+
+                   /   |     |  '.        S O N Y        .'  |  _  |   \\
+                  / ___| /|\\ |___ \\                   / ___| /_\\ |___ \\
+                 / |      |      | ;  __           _   ; | _         _ | ;
+                 | | <---   ---> | | |__|         |_:> | ||_|       (_)| |
+                 | |___   |   ___| ;SELECT       START ; |___       ___| ;
+                 |\\    | \\|/ |    /  _     ___      _   \\  | (X) |    /|
+                 | \\   |_____|  .','" "', |___|  ,'" "', '.  |_____|  .' |
+                 |  '-.______.-' /       \\ANALOG/       \\  '-._____.-'   |
+                 |               |       |------|         |               |
+                 |              /\\      /      \\       /\\              |
+                 |             /  '.___.'        '.___.'  \\             |
+                 |            /                            \\            |
+                 \\          /                              \\          /
+                  \\________/                                \\_________/
+                                    PS2 CONTROLLER
+                """;
     }
 }
