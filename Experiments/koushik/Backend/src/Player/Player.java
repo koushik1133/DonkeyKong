319056a724d9bfd8d245056111620472f2deecc6
Koushik Shaganti <koushik9@iastate.edu>
2024-10-08 17:31:02 -0500

    OneToMany

--- Experiments/koushik/Backend/src/Player/Player.java
+++ Experiments/koushik/Backend/src/Player/Player.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-//import src.Player;
+import src.Player;
 import src.PlayerRepository;
 public class Player {
 
