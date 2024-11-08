936b132df276a6caa8c1a4517ea2b191f2908644
smgumm <smgumm@iastate.edu>
2024-11-08 12:54:01 -0600

    working on countdown logic

---
+++ Backend/Multilayer_DK/src/main/java/backend/Countdown/WebSocketConfig.java
@@ -0,0 +1,21 @@
+package backend.Countdown;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.*;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Bean
+    public CountdownHandler countdownHandler() {
+        return new CountdownHandler();
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(countdownHandler(), "/countdown")
+                .setAllowedOrigins("*");
+    }
+}
