9979681f3fe6b049f083421f97b9704e6a7fdf4d
Koushik Shaganti <koushik9@iastate.edu>
2024-11-07 18:16:50 -0600

    Changed code of admin.class to work out with one to many.

--- Backend/Multilayer_DK/src/main/java/backend/Administrator/Admin.java
+++ Backend/Multilayer_DK/src/main/java/backend/Administrator/Admin.java
@@ -3,19 +3,16 @@ import backend.Players.Player;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.util.Set;
+//import javax.persistence.*;
+import java.util.List;
 
 /**
  * 
  * @author Sam Gumm
  */
-
+/*
 @Entity
 public class Admin {
-    //Getters and Setters for each field //
-    @Getter
-    @Setter
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -23,9 +20,10 @@ public class Admin {
     private String password;
     private String players;
 
-    @OneToOne(cascade = CascadeType.ALL)
+    @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "Admin_id")
     private Admin admin;
+
 
     public Admin(String name, String password) {
         this.name = name;
@@ -36,13 +34,13 @@ public class Admin {
 
     }
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
     public void setId(int id) {
         this.id = id;
     }
-    public String getname() {
+    public String getUsername() {
         return name;
     }
 
@@ -53,19 +51,47 @@ public class Admin {
         return password;
     }
 
-    public void setPassword(String password) {
+    public String setPassword(String password) {
         this.password = password;
     }
 
-    public Admin getAdmin() {
-        return admin;
-    }
+    //public String getAdmin() {return admin;}
 
-    public void setAdmin(Admin admin) {this.admin = admin;}
+    //public void setAdmin(Admin admin) {this.admin = admin;}
 
 
-    public String getPlayers() { return players; }
+    public void getPlayers() { return players; }
+
+
     public void setPlayers(Set<Player> players) {
         this.players = players.toString();
     }
+}
+*/
+@Setter
+@Getter
+@Entity
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String password;
+
+    // One-to-Many relationship: One Admin can have many Players
+    @OneToMany(mappedBy = "admin", cascade = CascadeType.ALL) // mappedBy refers to the 'admin' field in the Player class
+    private List<Player> players;
+
+    // Default constructor
+    public Admin() {
+    }
+
+    // Constructor with fields
+    public Admin(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
 }
