ff04657f2e47765471d5ca3cb814ced5f38f7eed
smgumm <smgumm@iastate.edu>
2024-11-08 12:54:01 -0600

    reworking db management

---
+++ Backend/Multilayer_DK/src/main/java/backend/Countdown/CountdownController.java
@@ -0,0 +1,22 @@
+package backend.Countdown;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/countdown")
+public class CountdownController {
+
+    private final CountdownHandler countdownHandler;
+
+    @Autowired
+    public CountdownController(CountdownHandler countdownHandler) {
+        this.countdownHandler = countdownHandler;
+    }
+
+    @PostMapping("/start")
+    public String startCountdown(@RequestParam(defaultValue = "120") int durationInSeconds) {
+        countdownHandler.startCountdown(durationInSeconds);
+        return "Countdown started for " + durationInSeconds + " seconds.";
+    }
+}
